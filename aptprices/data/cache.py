37a162a38f3b72092ac1cf9785dc506dcc775364
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-07-27 23:56:16 +0300

    Refactor to more functional form

--- aptprices/data/cache.py
+++ aptprices/data/cache.py
@@ -1,15 +1,20 @@
 """Data loading and caching
 
-TODO: Use clientz to download data. Requires loop since not all data can be
-      retrieved in one request. Cache.
+TODO: Postinumero column to string (leading zeros missing)
+TODO: Function to update all relevant caches
 
 """
 import json
 import os
 import pickle
+from typing import Callable, Iterable
 
 import attr
+import pandas as pd
+
 from clientz import paavo, statfin
+from aptprices import utils
+from aptprices.utils import compose
 
 
 CACHE = os.path.abspath(".aptprices-cache")
@@ -17,55 +22,128 @@ CACHE = os.path.abspath(".aptprices-cache")
 
 @attr.s(frozen=True)
 class Source():
+    """Generic data source
 
+    """
+
+    # Download data from a remote location
     download = attr.ib()
+
+    # Load data from disk cache
     load = attr.ib()
-    save = attr.ib()
 
-    def update(self, *args, **kwargs):
-        return self.save(self.download(*args, **kwargs))
+    # Download with saving
+    update = attr.ib()
 
 
-def PickleSource(filepath, download):
+def Landfill(filepath: str, download, dump, load):
 
-    def save(data):
-        with open(filepath, "wb+") as f:
-            pickle.dump(data, f)
-        return data
-
-    def load():
-        with open(filepath, "rb") as f:
-            data = pickle.load(f)
-        return data
+    # FIXME: api as input to download
+    @utils.mkdir(filepath)
+    def update():
+        return dump(download(), filepath)
 
     return Source(
         download=download,
-        load=load,
-        save=save
+        load=lambda: load(filepath),
+        update=update
     )
 
 
-def JSONSource(filepath, download):
+def Pickle(filepath, download):
 
-    def save(data):
-        with open(filepath, "w+") as f:
-            json.dump(data, f)
-        return data
+    def dump(x, y):
+        pickle.dump(x, open(y, "wb+"))
+        return x
 
-    def load():
-        with open(filepath, "r") as f:
-            data = json.load(f)
-        return data
+    def load(x):
+        return pickle.load(open(x, "rb"))
 
-    return Source(
-        download=download,
-        load=load,
-        save=save
-    )
+    return Landfill(filepath, download, dump, load)
+
+
+def JSON(filepath, download):
+
+    def dump(x, y):
+        json.dump(x, open(y, "w+"))
+        return x
+
+    def load(x):
+        return json.load(open(x, "r"))
+
+    return Landfill(filepath, download, dump, load)
+
+
+def lift(f: Callable):
+    """Lift a function
+
+    Example
+    -------
+
+    ..code-block :: python
+
+        triple = lift(lambda x: 3 * x)
+        Tripled = triple(Source)
+
+    """
+    def lifted(*sources):
+
+        return Source(
+            download=lambda: f(
+                *utils.tuplemap(lambda x: x.download())(sources)
+            ),
+            load=lambda: f(
+                *utils.tuplemap(lambda x: x.load())(sources)
+            ),
+            update=lambda: f(
+                *utils.tuplemap(lambda x: x.update())(sources)
+            )
+        )
+
+    return lifted
+
+
+def bind(f: Callable):
+    """Bind a function which returns a data source
+
+    """
+    def bound(*sources):
+        return Source(
+            download=f(
+                *utils.tuplemap(lambda x: x.download())(sources)
+            ).download,
+            load=f(
+                *utils.tuplemap(lambda x: x.load())(sources)
+            ).load,
+            update=f(
+                *utils.tuplemap(lambda x: x.update())(sources)
+            ).update
+        )
+
+    return bound
+
+
+def Concat(sources: Iterable, **kwargs):
+    """Concatenate sources
+
+    """
+
+    def concat(*frames, **kwargs):
+        return pd.concat(frames, **kwargs)
+
+    return lift(concat)(*sources)
+
+
+#
+# Data sources for the end-user
+#
 
 
 def YearlyMeta(filepath=os.path.join(CACHE, "yearly-meta.json")):
     """Metadata for yearly StatFin apartment prices
+
+    TODO: What is the difference between yearly and quarterly
+          metadata?
 
     """
     api = statfin.API()
@@ -73,7 +151,7 @@ def YearlyMeta(filepath=os.path.join(CACHE, "yearly-meta.json")):
     def download():
         return api.apartment_prices_yearly.get()
 
-    return JSONSource(filepath, download)
+    return JSON(filepath, download)
 
 
 def QuarterlyMeta(filepath=os.path.join(CACHE, "quarterly-meta.json")):
@@ -85,30 +163,45 @@ def QuarterlyMeta(filepath=os.path.join(CACHE, "quarterly-meta.json")):
     def download():
         return api.apartment_prices_quarterly.get()
 
-    return JSONSource(filepath, download)
+    return JSON(filepath, download)
 
 
-def Yearly(filepath=os.path.join(CACHE, "yearly.p")):
-    """Yearly StatFin apartment prices
-
-    TODO: Download all
+def YearlyZip(zip_code):
+    """Yearly apartment prices for a zip code area
 
     """
     api = statfin.API()
+    filepath = os.path.join(CACHE, zip_code, "yearly.p")
 
     def download(*args, **kwargs):
-        return api.apartment_prices_yearly.post(*args, **kwargs)
+        return api.apartment_prices_yearly.post(
+            query_code="Postinumero",
+            query_selection_values=[zip_code]
+        )
 
-    return PickleSource(filepath, download)
+    return Pickle(filepath, download)
 
 
-def Quarterly(filepath=os.path.join(CACHE, "quarterly.p")):
-    """Quarterly StatFin apartment prices
+def QuarterlyZip(zip_code):
+    """Quarterly apartment prices for a zip code area
 
     """
     api = statfin.API()
+    filepath = os.path.join(CACHE, zip_code, "yearly.p")
 
     def download(*args, **kwargs):
-        return api.apartment_prices_quarterly.post(*args, **kwargs)
+        return api.apartment_prices_quarterly.post(
+            query_code="Postinumero",
+            query_selection_values=[zip_code]
+        )
 
-    return PickleSource(filepath, download)
+    return Pickle(filepath, download)
+
+
+def Yearly():
+    """All yearly data
+
+    TODO: Use bind and metadata
+
+    """
+    return
